7c4b1835e0b98c2f29282d81c553c7703f2d0bfc
josansemat <joseantonio.segura-matute@iesruizgijon.com>
2025-03-09 12:56:19 +0100

    me sigue dando fallos de validacion

--- src/main/java/com/example/demo/model/Persona.java
+++ src/main/java/com/example/demo/model/Persona.java
@@ -1,7 +1,6 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,18 +13,12 @@ public abstract class Persona {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @NotBlank(message = "El nombre es obligatorio")
-    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     @Column(nullable = false)
     private String nombre;
     
-    @NotBlank(message = "Los apellidos son obligatorios")
-    @Size(min = 2, max = 100, message = "Los apellidos deben tener entre 2 y 100 caracteres")
     @Column(nullable = false)
     private String apellidos;
     
-    @Min(value = 15, message = "La edad mínima es 15 años")
-    @Max(value = 50, message = "La edad máxima es 50 años")
     private int edad;
     
     private String nacionalidad = "España";
